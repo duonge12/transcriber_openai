e83dfd71d8b22f794536c72edfbea2d59f83a73a
duonge12 <duonge12@gmail.com>
2025-04-02 05:56:48 +0700

    test port

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from controller.transcriber_controller import get_script_controller
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def getScript():
     data = request.get_json()
-    if data and 'url' in data :
+    if data and 'url' in data:
         return get_script_controller(data['url'])
     else:
         return 'Invalid request', 400
@@ -16,4 +17,5 @@ def hello():
     return "Nhung"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False for production
