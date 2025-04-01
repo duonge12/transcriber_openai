27722a3f8cc63f5b8494de6debb5f9bd795a2dcf
duonge12 <duonge12@gmail.com>
2025-04-02 04:08:39 +0700

    testing endpoint

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from controller.transcriber_controller import get_script_controller
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Worlffd!'
+@app.route('/', methods=['POST'])
+def getScript():
+    data = request.get_json()
+    if data and 'url' in data :
+        return get_script_controller(data['url'])
+    else:
+        return 'Invalid request', 400
 
+@app.route('/')
+def hello():
+    return "Nhung"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model/transcriber.py
+++ model/transcriber.py
@@ -6,7 +6,7 @@ model = whisper.load_model("base")
 class TranscriberModel:
     @staticmethod
     def getScript(path):
-        result = model.transcribe(path, language="vi")
+        result = model.transcribe(path, language="en")
         return result
 
 
