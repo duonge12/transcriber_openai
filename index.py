f62a13026ae73f261cddeea5e1ee59286b7e1be2
duonge17 <nhung.nguyen@digiex.group>
2025-04-02 18:15:06 +0700

    add post method

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
-from flask import Flask, jsonify
-
+from flask import Flask,request, jsonify
+from controller.transcriber_controller import get_script_controller
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from Flask on Vercel!"})
+    return jsonify({"message": "Nhung!"})
+
+@app.route('/', methods=['POST'])
+def getTranscript():
+    if request.method == 'POST':
+        # Get JSON data from the request body
+        data = request.get_json()
+        
+        result=get_script_controller(data['url']);
+        
+        # Return the response with the received message
+        return jsonify(result)
+    
+    return jsonify({"message": "Nhung!"})
 
 if __name__ == '__main__':
     from os import environ
